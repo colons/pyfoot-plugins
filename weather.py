46214b7b5535d0bf58966b1a8dbe5c8b8ed2082f
Iain Dawson <iain@bldm.us>
2012-05-21 20:53:50 +0100

    Automatic degC conversion

--- weather.py
+++ weather.py
@@ -5,6 +5,9 @@ import urllib2
 from urllib import urlencode
 from xml.dom import minidom
 
+def f_to_c(f):
+    return (f - 32) * (5./9.)
+
 class Plugin(plugin.Plugin):
     def register_commands(self):
         self.commands = [('weather current <<location>>', self.current),
@@ -35,7 +38,7 @@ class Plugin(plugin.Plugin):
     def forecast(self, message, args):
         """ Fetches the weather forecast for <span class="irc"><span class="repl">location</span></span>.
         $<comchar>w f nantwich and crewe
-        >\x02Crewe, Cheshire East\x02 \x03#|\x03 \x02Mon\x02 \x03#:\x03 high 70°F \x03#:\x03 low 48°F \x03#:\x03 Partly Sunny \x03#|\x03 \x02Tue\x02 \x03#:\x03 high 75°F \x03#:\x03 low 50°F \x03#:\x03 Clear \x03#|\x03 \x02Wed\x02 \x03#:\x03 high 82°F \x03#:\x03 low 59°F \x03#:\x03 Partly Sunny \x03#|\x03 \x02Thu\x02 \x03#:\x03 high 77°F \x03#:\x03 low 52°F \x03#:\x03 Cloudy
+        >\x02Crewe, Cheshire East\x02 \x03#|\x03 \x02mon\x02 \x03#:\x03 high 70°F 21°C \x03#:\x03 low 48°F 8°C \x03#:\x03 partly sunny \x03#|\x03 \x02tue\x02 \x03#:\x03 high 75°F 23°C \x03#:\x03 low 50°F 10°C \x03#:\x03 clear \x03#|\x03 \x02wed\x02 \x03#:\x03 high 82°F 27°C \x03#:\x03 low 59°F 15°C \x03#:\x03 partly sunny \x03#|\x03 \x02thu\x02 \x03#:\x03 high 77°F 25°C \x03#:\x03 low 52°F 11°C \x03#:\x03 cloudy
         """
         
         url = self.url + urlencode({"weather": args["location"]})
@@ -45,13 +48,15 @@ class Plugin(plugin.Plugin):
         msg = u'\x02%s\x02' % dom.getElementsByTagName("city")[0].getAttribute("data")
 
         conditions = dom.getElementsByTagName("forecast_conditions")
-        conditions_str = u" \x03#|\x03 \x02%s\x02 \x03#:\x03 high %s°F \x03#:\x03 low %s°F \x03#:\x03 %s"
+        conditions_str = u" \x03#|\x03 \x02%s\x02 \x03#:\x03 high %s°F %s°C \x03#:\x03 low %s°F %s°C \x03#:\x03 %s"
 
         for elements in conditions:
             day = elements.getElementsByTagName("day_of_week")[0].getAttribute("data")
             high_f = elements.getElementsByTagName("high")[0].getAttribute("data")
+            high_c = u'%i' % f_to_c(int(high_f))
             low_f = elements.getElementsByTagName("low")[0].getAttribute("data")
+            low_c = u'%i' % f_to_c(int(low_f))
             condition = elements.getElementsByTagName("condition")[0].getAttribute("data")
-            msg += conditions_str % (day.lower(), high_f, low_f, condition.lower())
+            msg += conditions_str % (day.lower(), high_f, high_c, low_f, low_c, condition.lower())
 
         self.irc.privmsg(message.source, msg)
