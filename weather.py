ffd9327cd57a900317d795ac185361d3d465ca0a
Ensign Tropic <ensnrc@gmx.com>
2012-05-27 16:47:12 -0500

    Removed dependence on source code character set.

--- weather.py
+++ weather.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import plugin
 import urllib2
 from urllib import urlencode
@@ -15,17 +13,18 @@ class Plugin(plugin.Plugin):
 
     def prepare(self):
         self.url = "http://www.google.com/ig/api?"
-        
+
     def current(self, message, args):
-        """ Fetches the current weather in <span class="irc"><span class="repl">location</span></span>.
+        u""" Fetches the current weather in <span class="irc"><span class="repl">location</span></span>.
         $<comchar>w c stafford, uk
-        >\x02Stafford, Staffordshire\x02 \x03#|\x03 61°F \x03#:\x03 16°C \x03#|\x03 humidity \x03#:\x03 68% \x03#|\x03 wind \x03#:\x03 W at 9 mph"""
-        
+        >\x02Stafford, Staffordshire\x02 \x03#|\x03 61\u00b0F \x03#:\x03 16\u00b0C \x03#|\x03 humidity \x03#:\x03 68% \x03#|\x03 wind \x03#:\x03 W at 9 mph
+        """
+
         url = self.url + urlencode({"weather":args["location"]})
         data = urllib2.urlopen(url)
         dom = minidom.parse(data)
 
-        msg = u"\x02%s\x02 \x03#|\x03 %s°F \x03#:\x03 %s°C \x03#|\x03 humidity \x03#:\x03 %s \x03#|\x03 wind \x03#:\x03 %s"
+        msg = u"\x02%s\x02 \x03#|\x03 %s\u00b0F \x03#:\x03 %s\u00b0C \x03#|\x03 humidity \x03#:\x03 %s \x03#|\x03 wind \x03#:\x03 %s"
 
         city = dom.getElementsByTagName("city")[0].getAttribute("data")
         tempf = dom.getElementsByTagName("temp_f")[0].getAttribute("data")
@@ -37,11 +36,11 @@ class Plugin(plugin.Plugin):
         self.irc.privmsg(message.source, msg)
 
     def forecast(self, message, args):
-        """ Fetches the weather forecast for <span class="irc"><span class="repl">location</span></span>.
+        u""" Fetches the weather forecast for <span class="irc"><span class="repl">location</span></span>.
         $<comchar>w f nantwich and crewe
-        >\x02Crewe, Cheshire East\x02 \x03#|\x03 \x02mon\x02 \x03#:\x03 high 70°F 21°C \x03#:\x03 low 48°F 8°C \x03#:\x03 partly sunny \x03#|\x03 \x02tue\x02 \x03#:\x03 high 75°F 23°C \x03#:\x03 low 50°F 10°C \x03#:\x03 clear \x03#|\x03 \x02wed\x02 \x03#:\x03 high 82°F 27°C \x03#:\x03 low 59°F 15°C \x03#:\x03 partly sunny \x03#|\x03 \x02thu\x02 \x03#:\x03 high 77°F 25°C \x03#:\x03 low 52°F 11°C \x03#:\x03 cloudy
+        >\x02Crewe, Cheshire East\x02 \x03#|\x03 \x02mon\x02 \x03#:\x03 high 70\u00b0F 21\u00b0C \x03#:\x03 low 48\u00b0F 8\u00b0C \x03#:\x03 partly sunny \x03#|\x03 \x02tue\x02 \x03#:\x03 high 75\u00b0F 23\u00b0C \x03#:\x03 low 50\u00b0F 10\u00b0C \x03#:\x03 clear \x03#|\x03 \x02wed\x02 \x03#:\x03 high 82\u00b0F 27\u00b0C \x03#:\x03 low 59\u00b0F 15\u00b0C \x03#:\x03 partly sunny \x03#|\x03 \x02thu\x02 \x03#:\x03 high 77\u00b0F 25\u00b0C \x03#:\x03 low 52\u00b0F 11\u00b0C \x03#:\x03 cloudy
         """
-        
+
         url = self.url + urlencode({"weather": args["location"]})
         data = urllib2.urlopen(url)
         dom = minidom.parse(data)
@@ -49,7 +48,7 @@ class Plugin(plugin.Plugin):
         msg = u'\x02%s\x02' % dom.getElementsByTagName("city")[0].getAttribute("data")
 
         conditions = dom.getElementsByTagName("forecast_conditions")
-        conditions_str = u" \x03#|\x03 \x02%s\x02 \x03#:\x03 high %s°F %s°C \x03#:\x03 low %s°F %s°C \x03#:\x03 %s"
+        conditions_str = u" \x03#|\x03 \x02%s\x02 \x03#:\x03 high %s\u00b0F %s\u00b0C \x03#:\x03 low %s\u00b0F %s\u00b0C \x03#:\x03 %s"
 
         for elements in conditions:
             day = elements.getElementsByTagName("day_of_week")[0].getAttribute("data")
