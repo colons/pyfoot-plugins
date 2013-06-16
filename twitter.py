505a1aefdd113b2750ab3d2afd289e1f93423776
Iain Dawson <iain@bldm.us>
2013-06-16 14:43:02 +0100

    Improve the twitter plugin.

--- twitter.py
+++ twitter.py
@@ -11,24 +11,62 @@ defaults = {
 
 
 class Plugin(plugin.Plugin):
+    shelf_required = True
+
     def postfork(self):
         self.tw_api = Twitter(auth=OAuth(**self.conf['twitter_oath']))
 
     def register_commands(self):
         self.commands = [
-            ('twitter <screen_name>', self.user),
+            ('twitter <<query>>', self.query),
+            ('twitter set <screen_name>', self.set_user),
+            ('twitter', self.self),
         ]
 
-    def user(self, message, args):
+    def query(self, message, args):
         """
-        Get the latest tweet from a user.
+        Get the latest tweet from a user or search with either a hashtag or a
+        multi-word search string.
         """
 
-        latest = self.tw_api.statuses.user_timeline(
-            screen_name=args['screen_name'])[0]
+        query = args['query']
+
+        if query.startswith('#') or ' ' in query:
+            tweet = self.tw_api.search.tweets(q=query)['statuses'][0]
+
+        else:
+            screen_name = self.shelf.get(query.lower(), query.lower())
+
+            tweet = self.tw_api.statuses.user_timeline(
+                screen_name=screen_name)[0]
+
+        self.send_tweet(message, tweet)
+
+    def send_tweet(self, message, tweet):
         self.send_struc(message.source,
-                        (latest['text'],
+                        (tweet['text'],
                          'http://twitter.com/%s/status/%s'
-                         % (latest['user']['screen_name'],
-                            latest['id_str']))
+                         % (tweet['user']['screen_name'],
+                            tweet['id_str']))
                         )
+
+    def set_user(self, message, args):
+        """
+        Tell <pyfoot> who you are.
+        """
+
+        name = args['screen_name']
+        self.tw_api.users.show(screen_name=name)  # will error if not a user
+        self.shelf[message.nick.lower()] = name
+        self.shelf.sync()
+        self.send_struc(message.source,
+                        ('\x02%s\x02 is now Twitter user \x02%s\x02'
+                         % (message.nick, name),
+                         'http://twitter.com/%s' % name))
+
+    def self(self, message, args):
+        """
+        Get your latest tweet.
+        """
+
+        self.query(message, {'query': message.nick})
